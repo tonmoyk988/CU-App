9e5f451b0d040635c9dfddb07080812501e056d1
tonmoy <tonmoyk988@gmail.com>
2019-04-06 13:29:36 +0600

    no summary
    developed by tonmoy

---
+++ CUApp/app/src/main/java/com/hfad/cuapp/dept_update.java
@@ -0,0 +1,21 @@
+package com.hfad.cuapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+//import android.v7.widget.Toolbar;
+
+public class dept_update extends AppCompatActivity {
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dept_update);
+
+        toolbar = (Toolbar) findViewById(R.id.update_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Updates");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
